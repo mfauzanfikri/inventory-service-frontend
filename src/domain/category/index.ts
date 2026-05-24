438df0763030aa0d941980cdb0e49454578fa6a7
mfauzanfikri <mfauzanfikri09@gmail.com>
2026-05-24 15:03:28 +0700

    refactor(domain): rename category repository contract to match filename

--- src/domain/category/index.ts
+++ src/domain/category/index.ts
@@ -2,4 +2,4 @@ export * from "./category.entity";
 export * from "./category-name";
 export * from "./category-description";
 export * from "./category.errors";
-export * from "./category.repository";
+export * from "./category-domain.repository";
