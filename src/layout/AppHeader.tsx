cbb738de7620a7355b840f291cde978fc07cba82
mfauzanfikri <mfauzanfikri09@gmail.com>
2026-06-01 18:35:48 +0700

    feat(branding): replace template assets with custom designed My Inventory SVG logos

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -85,15 +85,15 @@ const AppHeader: React.FC = () => {
 
           <Link href="/" className="lg:hidden">
             <Image
-              width={154}
-              height={32}
+              width={180}
+              height={38}
               className="dark:hidden"
               src="./images/logo/logo.svg"
               alt="Logo"
             />
             <Image
-              width={154}
-              height={32}
+              width={180}
+              height={38}
               className="hidden dark:block"
               src="./images/logo/logo-dark.svg"
               alt="Logo"
--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -273,14 +273,14 @@ const AppSidebar: React.FC = () => {
                 className="dark:hidden"
                 src="/images/logo/logo.svg"
                 alt="Logo"
-                width={150}
+                width={180}
                 height={40}
               />
               <Image
                 className="hidden dark:block"
                 src="/images/logo/logo-dark.svg"
                 alt="Logo"
-                width={150}
+                width={180}
                 height={40}
               />
             </>
